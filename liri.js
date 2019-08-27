cf5a6fedd903e9b63a28c151c84cccf77bc929e5
Artur Markov <artur.markov1860@gmail.com>
2019-08-26 21:04:59 -0700

    movie this if no movie

--- liri.js
+++ liri.js
@@ -27,16 +27,29 @@ if (command === "spotify-this-song") {
 } else if (command === "concert-this") {
 
 } else if (command === "movie-this") {
-    axios.get("https://www.omdbapi.com/?t="+item+"&y=&plot=short&apikey=trilogy").then((response) => {
-        console.log("Title: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-    })
+    if (item) {
+        axios.get("https://www.omdbapi.com/?t=" + item + "&y=&plot=short&apikey=trilogy").then((response) => {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+    } else {
+        axios.get("https://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then((response) => {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
+    }
 } else if (command === "do-what-it-says") {
 
 } else {

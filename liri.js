56fd5cedbcf80dd3ee7cf07b6bc04cabef7dbf85
Artur Markov <artur.markov1860@gmail.com>
2019-08-27 13:26:11 -0700

    comments added

--- liri.js
+++ liri.js
@@ -1,21 +1,34 @@
+
+//add dotenv package for hiding private api keys
 require("dotenv").config();
 const keys = require("./keys.js");
 
+//spotify npm package
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
 
+//files system npm pakage
 const fs = require("fs");
 
+//package for making requests
 const axios = require("axios");
+
+//format date npm pakage
 const moment = require("moment");
 
 
-
+//declare command and item that will be used
 let command = process.argv[2];
 let item = process.argv[3];
 
+
+//liri bot funciton
 function liri() {
+
+    //command spotify-this-song
     if (command === "spotify-this-song") {
+
+        //if user put a track, liri will search this track
         if (item) {
             spotify.search({ type: 'track', query: item, limit: 1 }, function (err, data) {
                 if (err) {
@@ -29,6 +42,7 @@ function liri() {
                 console.log("=======================");
             });
 
+            //if user didn't put a track, liri will give default information
         } else {
             spotify.search({ type: 'track', query: "The Sign", limit: 3}, function (err, data) {
                 if (err) {
@@ -42,26 +56,34 @@ function liri() {
                     console.log("=======================");
             });
         }
+
+        //command concert-this will give information about venues of artist that we entered
     } else if (command === "concert-this") {
 
-        axios.get("https://rest.bandsintown.com/artists/" + item + "/events?app_id=codingbootcamp").then((response) => {
-            if (response.data.length == 0) {
-                console.log("Sorry, this artist doesn't have upcoming events");
-            } else {
-                for (var i = 0; i < response.data.length; i++) {
-                    console.log("=======================");
-                    console.log("Venue: " + response.data[i].venue.name);
-                    console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-                    console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                    console.log("=======================");
+        if (item) {
+            axios.get("https://rest.bandsintown.com/artists/" + item + "/events?app_id=codingbootcamp").then((response) => {
+                if (response.data.length == 0) {
+                    console.log("Sorry, this artist doesn't have upcoming events");
+                } else {
+                    for (var i = 0; i < response.data.length; i++) {
+                        console.log("=======================");
+                        console.log("Venue: " + response.data[i].venue.name);
+                        console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
+                        console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                        console.log("=======================");
+                    }
                 }
-            }
-        }).catch((err) => {
-            if (err) {
-                console.log(err.message);
-            }
-        })
+            }).catch((err) => {
+                if (err) {
+                    console.log(err.message);
+                }
+            })
+        } else {
+            console.log("Please write name of artist");
+        }
 
+
+            //command movie-this will give information about movie that user needs
     } else if (command === "movie-this") {
         if (item) {
             axios.get("https://www.omdbapi.com/?t=" + item + "&y=&plot=short&apikey=trilogy").then((response) => {
@@ -76,6 +98,8 @@ function liri() {
                 console.log("Actors: " + response.data.Actors);
                 console.log("=======================");
             })
+
+            //if user didn't put any movie, by default liri will give information about "Mr.Nobody" movie
         } else {
             axios.get("https://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy").then((response) => {
                 console.log("=======================");
@@ -90,6 +114,8 @@ function liri() {
                 console.log("=======================");
             })
         }
+
+        //command do-what-it-says will give user default command and item
     } else if (command === "do-what-it-says") {
 
         var contents = fs.readFileSync('./random.txt', 'utf8');
@@ -97,6 +123,7 @@ function liri() {
         item = contents.slice(contents.indexOf(",") + 2);
         liri();
 
+            //if user put command that wasn't recognized by liri, user will get this message
     } else {
         console.log("=======================");
         console.log("Sorry, the command is not recognized. Please try following commands: 'concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'")
@@ -104,4 +131,5 @@ function liri() {
     }
 }
 
+//executing the liri bot function
 liri();
